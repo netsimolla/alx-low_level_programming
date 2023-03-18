8f446252780329cb720466081aebccfd95b8fc12
netsa net <netsanetmolla567@gmail.com>
2023-03-18 01:28:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - - Dora Korpar
- * followed by a new line, to standard error.
- * Return: 1 always
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
