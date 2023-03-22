bb0f4d2a2a9fadfbfa34146e30cb335879ceadc8
netsa net <netsanetmolla567@gmail.com>
2023-03-22 03:48:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+putchar(c[i]);
+}
+putchar('\n');
+return (0);
+}
